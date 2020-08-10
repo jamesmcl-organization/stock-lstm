44950a62ede9b379333a4c042e466eb2b6510cbe
James McLoughlin <mcloughlin_james@yahoo.com>
2020-08-10 15:26:02 -0700

    Schedule Cron updated

--- Desktop/Data_Science/stock_lstm/stock_lstm_pycharm/scheduleCron.py
+++ Desktop/Data_Science/stock_lstm/stock_lstm_pycharm/scheduleCron.py
@@ -1,3 +1,11 @@
 from crontab import CronTab
+my_cron = CronTab(user=True)
 
-cron = CronTab(user='root')
+
+job = my_cron.new(command='python /Users/jamesm/Desktop/Data_Science/stock_lstm/stock_lstm_pycharm/prepare_stock_data.py')
+
+job.minute.every(1)
+
+job.minute.every(1)
+
+my_cron.write()
