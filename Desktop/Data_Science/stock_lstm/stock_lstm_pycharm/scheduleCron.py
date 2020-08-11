981dd48561e38daa7ca2d8c09e2593f27e3282b4
James McLoughlin <mcloughlin_james@yahoo.com>
2020-08-10 17:10:51 -0700

    Scheduled Cron jog to run Mon-Fri at 4pm

--- Desktop/Data_Science/stock_lstm/stock_lstm_pycharm/scheduleCron.py
+++ Desktop/Data_Science/stock_lstm/stock_lstm_pycharm/scheduleCron.py
@@ -1,11 +1,17 @@
 from crontab import CronTab
-my_cron = CronTab(user=True)
+import datetime
 
+my_cron = CronTab (user=True)
 
-job = my_cron.new(command='python /Users/jamesm/Desktop/Data_Science/stock_lstm/stock_lstm_pycharm/prepare_stock_data.py')
+job = my_cron.new (
+    command='python /Users/jamesm/Desktop/Data_Science/stock_lstm/stock_lstm_pycharm/prepare_stock_data.py',
+    comment='Daily Stock Information')
 
-job.minute.every(1)
+#Schedule Job for Monday-Friday at 4pm
+job.setall('00 16 * * 1-5')
 
-job.minute.every(1)
+my_cron.write ()
 
-my_cron.write()
+for job in my_cron:
+    sch = job.schedule (date_from=datetime.datetime.now ())
+    print (sch.get_next ())
